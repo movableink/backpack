e79ac0a764c003e7c132d7e640f66749924edbb3
Peter Lo <plo@Peter-Los-MacBook-Pro.local>
2017-01-19 16:34:01 -0500

    Added cancel and handlefallback methods

--- src/backpack.js
+++ src/backpack.js
@@ -7,6 +7,32 @@ export default class Backpack {
   }
 
   /**
+   * @param {string} msg
+   * create error so we can have a stack trace
+   */
+  cancel(msg) {
+      this.message = msg;
+      this.stack = (new Error(msg)).stack;
+      this.handleFallback({message : this.message, stack : this.stack});
+  }
+  ​
+  handleFallback(e){
+    /*
+     *  Determine the type of error we just had
+     *  Was it something we were looking for i.e not enough items (in which case it is a cancelRequest)
+     *  Or was it an error we didn't catch i.e API returned non JSON information when we were expecting JSON (in which case we throw an error, which in turns notifies Dev)
+     */
+    if(e instanceof Error) {
+      console.log(e);
+      //CD.throwError(e);
+    } else {
+      console.log(e.message);
+      console.log(e.stack);
+      CD.cancelRequest(e.message);
+    }
+  }
+
+  /**
    * Triggers the fallback image to show
    * @param msg
    */
